b2f9897313461c54978137fcc700df5c413b7c8d
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-10-16 15:45:52 -0400

    feat: finish ex_4.dart

--- part1/ex_4.dart
+++ part1/ex_4.dart
@@ -1,3 +1,37 @@
 // Create an Enum WeekDay with all the days of the week. 
 // Define an extention over int that returns true if the number is even. 
-// Use t to verify if the number entered by the user is even.
+// Use it to verify if the number entered by the user is even.
+
+import 'dart:io';
+
+// Define the WeekDay enum
+enum WeekDay { 
+  Monday, 
+  Tuesday, 
+  Wednesday, 
+  Thursday, 
+  Friday, 
+  Saturday, 
+  Sunday 
+}
+
+// Define an extension on int to check if a number is even
+extension EvenChecker on int {
+  bool isEven() {
+    return this % 2 == 0;
+  }
+}
+
+void main() {
+  print("Enter a number:");
+  var input = stdin.readLineSync();
+  var intInput = int.parse(input!);
+
+  if(intInput.isEven){
+    print("${intInput} is even");
+  }
+  else {
+    print("${intInput} is odd");
+  }
+
+}
